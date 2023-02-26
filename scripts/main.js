a7d223c9099340ce3d0b2626cb0f64afc1131cc4
Michelwas <mgwassing@gmail.com>
2023-02-26 23:23:02 +0100

    added: new approach of API usage and added more styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,27 @@
 var storyObject = [{ id: 1,
     title: "De Kikker en de Os",
     language: "Dutch",
-    imgUrl: "",
+    imgUrl: "./images/story-thumbnail.jpg",
     summary: "Een Os kwam naar een rietachtig poel om te drinken. Toen hij zwaar in het water plonsde, trapte hij een jonge Kikker in de modder. De oude Kikker miste het jonge diertje snel en vroeg aan zijn broers en zussen wat er van hem was geworden. &ldquo;Een grote monster,&rdquo; zei een van hen, &ldquo;stepte op onze kleine broer met een van zijn enorme voeten!&rdquo; &ldquo;Groot was hij!&rdquo; zei de oude Kikker, zich opblazend. &ldquo;Was hij zo groot als dit?&rdquo; &ldquo;Oh, veel groter!&rdquo; riepen ze. De Kikker blies zich nog meer op. &ldquo;Hij kon niet groter zijn dan dit,&rdquo; zei ze. Maar de kleine Kikkers verklaarden allemaal dat het monster veel, veel groter was en de oude Kikker bleef zich steeds meer opblazen totdat ze opeens barstte."
 }, { id: 2,
     title: "De Kikker en de Os",
     language: "Dutch",
-    imgUrl: "",
+    imgUrl: "./images/story-thumbnail.jpg",
     summary: "Een Os kwam naar een rietachtig poel om te drinken. Toen hij zwaar in het water plonsde, trapte hij een jonge Kikker in de modder. De oude Kikker miste het jonge diertje snel en vroeg aan zijn broers en zussen wat er van hem was geworden. &ldquo;Een grote monster,&rdquo; zei een van hen, &ldquo;stepte op onze kleine broer met een van zijn enorme voeten!&rdquo; &ldquo;Groot was hij!&rdquo; zei de oude Kikker, zich opblazend. &ldquo;Was hij zo groot als dit?&rdquo; &ldquo;Oh, veel groter!&rdquo; riepen ze. De Kikker blies zich nog meer op. &ldquo;Hij kon niet groter zijn dan dit,&rdquo; zei ze. Maar de kleine Kikkers verklaarden allemaal dat het monster veel, veel groter was en de oude Kikker bleef zich steeds meer opblazen totdat ze opeens barstte."
 }, { id: 3,
     title: "De Kikker en de Os",
     language: "Dutch",
-    imgUrl: "",
+    imgUrl: "./images/story-thumbnail.jpg",
     summary: "Een Os kwam naar een rietachtig poel om te drinken. Toen hij zwaar in het water plonsde, trapte hij een jonge Kikker in de modder. De oude Kikker miste het jonge diertje snel en vroeg aan zijn broers en zussen wat er van hem was geworden. &ldquo;Een grote monster,&rdquo; zei een van hen, &ldquo;stepte op onze kleine broer met een van zijn enorme voeten!&rdquo; &ldquo;Groot was hij!&rdquo; zei de oude Kikker, zich opblazend. &ldquo;Was hij zo groot als dit?&rdquo; &ldquo;Oh, veel groter!&rdquo; riepen ze. De Kikker blies zich nog meer op. &ldquo;Hij kon niet groter zijn dan dit,&rdquo; zei ze. Maar de kleine Kikkers verklaarden allemaal dat het monster veel, veel groter was en de oude Kikker bleef zich steeds meer opblazen totdat ze opeens barstte."
 }, { id: 4,
     title: "De Kikker en de Os",
     language: "Dutch",
-    imgUrl: "",
+    imgUrl: "./images/story-thumbnail.jpg",
     summary: "Een Os kwam naar een rietachtig poel om te drinken. Toen hij zwaar in het water plonsde, trapte hij een jonge Kikker in de modder. De oude Kikker miste het jonge diertje snel en vroeg aan zijn broers en zussen wat er van hem was geworden. &ldquo;Een grote monster,&rdquo; zei een van hen, &ldquo;stepte op onze kleine broer met een van zijn enorme voeten!&rdquo; &ldquo;Groot was hij!&rdquo; zei de oude Kikker, zich opblazend. &ldquo;Was hij zo groot als dit?&rdquo; &ldquo;Oh, veel groter!&rdquo; riepen ze. De Kikker blies zich nog meer op. &ldquo;Hij kon niet groter zijn dan dit,&rdquo; zei ze. Maar de kleine Kikkers verklaarden allemaal dat het monster veel, veel groter was en de oude Kikker bleef zich steeds meer opblazen totdat ze opeens barstte."
 }, { id: 5,
     title: "De Kikker en de Os",
     language: "Dutch",
-    imgUrl: "",
+    imgUrl: "./images/story-thumbnail.jpg",
     summary: "Een Os kwam naar een rietachtig poel om te drinken. Toen hij zwaar in het water plonsde, trapte hij een jonge Kikker in de modder. De oude Kikker miste het jonge diertje snel en vroeg aan zijn broers en zussen wat er van hem was geworden. &ldquo;Een grote monster,&rdquo; zei een van hen, &ldquo;stepte op onze kleine broer met een van zijn enorme voeten!&rdquo; &ldquo;Groot was hij!&rdquo; zei de oude Kikker, zich opblazend. &ldquo;Was hij zo groot als dit?&rdquo; &ldquo;Oh, veel groter!&rdquo; riepen ze. De Kikker blies zich nog meer op. &ldquo;Hij kon niet groter zijn dan dit,&rdquo; zei ze. Maar de kleine Kikkers verklaarden allemaal dat het monster veel, veel groter was en de oude Kikker bleef zich steeds meer opblazen totdat ze opeens barstte."
 }];
 
@@ -35,54 +35,102 @@ window.addEventListener("load", () => {
 
     
     storyObject.map((story) => {
+        // create story list item
         const storyListItem = document.createElement("li");
         storyListItem.className = "story-list-item";
         storyList.appendChild(storyListItem);
       
+        // create story image
         const storyImg = document.createElement("img");
         storyImg.className = "story-img";
         storyImg.src = story.imgUrl;
         storyListItem.appendChild(storyImg);
       
-        const storyTitle = document.createElement("h3");
-        storyTitle.className = "story-title";
-        storyTitle.innerHTML = story.title;
-        storyListItem.appendChild(storyTitle);
+        // create story info container
+        const storyInfo = document.createElement("div");
+        storyInfo.className = "story-info";
+        storyListItem.appendChild(storyInfo);
+
+            // create story title and append to info container
+            const storyTitle = document.createElement("h3");
+            storyTitle.className = "story-title";
+            storyTitle.innerHTML = story.title;
+            storyInfo.appendChild(storyTitle);
       
-        const storySummary = document.createElement("p");
-        storySummary.className = "story-summary";
-        storySummary.innerHTML = story.summary;
-        storyListItem.appendChild(storySummary);
+            // create story summary and append to info container
+            const storySummary = document.createElement("p");
+            storySummary.className = "story-summary";
+            storySummary.innerHTML = story.summary;
+            storyInfo.appendChild(storySummary);
+
+        // create story button append to list item
+        const storyButton = document.createElement("button");
+        storyButton.className = "story-button";
+        storyListItem.appendChild(storyButton);
+
+        // create story svg appearance and append to button
+        const iconSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        const iconPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        const iconCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+        iconSvg.classList.add('story-button-appearance');
+        iconSvg.setAttribute("fill", "none");
+        iconSvg.setAttribute("viewBox", "0 0 146 146");
+        iconSvg.setAttribute("width", "146");
+        iconSvg.setAttribute("height", "146");
+
+        iconPath.setAttribute(
+            "d",
+            "M57.0972 109.209C55.0372 110.511 52.9525 110.587 50.8431 109.437C48.7337 108.287 47.677 106.508 47.6729 104.101V41.9039C47.6729 39.5002 48.7296 37.7214 50.8431 36.5676C52.9567 35.4139 55.0413 35.49 57.0972 36.796L107.463 67.8943C109.317 69.0962 110.244 70.7988 110.244 73.0023C110.244 75.2057 109.317 76.9083 107.463 78.1102L57.0972 109.209Z"
+          );
+        iconPath.setAttribute("fill", "#FF7151");
+
+        iconCircle.setAttribute("cx", "73");
+        iconCircle.setAttribute("cy", "73");
+        iconCircle.setAttribute("r", "72");
+        iconCircle.setAttribute("stroke", "#FF7151");
+        iconCircle.setAttribute("stroke-width", "2");
+
+        iconSvg.appendChild(iconPath);
+        iconSvg.appendChild(iconCircle);
+        storyButton.appendChild(iconSvg);
       });
 });
 
 const synth = window.speechSynthesis;
 
-// Add a click event listener to the <p> element
-document.addEventListener("click", (e) => {
-        // Get the text content of the <p> element
-        const text = e.target.textContent;
-        // TODO: change target paragrapth class to button when its added
-        const targetParagraph = document.querySelector(".story-summary");
+window.addEventListener('load', () => {
+    const storyList = document.querySelector('.story-list');
 
-        if (e.target != targetParagraph && synth.speaking) {
+  if (storyList) {
+    storyList.addEventListener('click', (e) => {
+      const listItem = e.target.closest('.story-list-item');
+
+      // Check if synth is speaking
+      if (synth.speaking) {
             synth.cancel();
             return;
-        }
-        
-        if (e.target == targetParagraph) {
-            const utterThis = new SpeechSynthesisUtterance(text);
-        
-            utterThis.onend = (event) => {
-              console.log("SpeechSynthesisUtterance.onend");
-            };
-        
-            utterThis.onerror = (event) => {
-              console.error("SpeechSynthesisUtterance.onerror" + " " + event.error);
-            };
+      }
 
-            synth.speak(utterThis);
-          } else {
+      if (listItem) {
+        const listItems = Array.from(storyList.children);
+        const index = listItems.indexOf(listItem) + 1;
+
+        const story = storyObject.find((story) => story.id === index);
+        const summaryUtterance = new SpeechSynthesisUtterance(story.summary);
+
+        summaryUtterance.onend = (event) => {
+            console.log("SpeechSynthesisUtterance.onend");
+          };
+      
+        summaryUtterance.onerror = (event) => {
+            console.error("SpeechSynthesisUtterance.onerror" + " " + event.error);
+          };
+
+        synth.speak(summaryUtterance);
+      } else{
             console.log("Not a story summary");
-        }     
+      }
+    });
+  }
 });
