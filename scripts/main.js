bd9706d8976962933698410e0767bddc7ed350fb
Michelwas <mgwassing@gmail.com>
2023-02-27 01:02:30 +0100

    added: refactored code to fit es6 modules syntax

--- scripts/main.js
+++ scripts/main.js
@@ -1,136 +1,38 @@
-var storyObject = [{ id: 1,
-    title: "De Kikker en de Os",
-    language: "Dutch",
-    imgUrl: "./images/story-thumbnail.jpg",
-    summary: "Een Os kwam naar een rietachtig poel om te drinken. Toen hij zwaar in het water plonsde, trapte hij een jonge Kikker in de modder. De oude Kikker miste het jonge diertje snel en vroeg aan zijn broers en zussen wat er van hem was geworden. &ldquo;Een grote monster,&rdquo; zei een van hen, &ldquo;stepte op onze kleine broer met een van zijn enorme voeten!&rdquo; &ldquo;Groot was hij!&rdquo; zei de oude Kikker, zich opblazend. &ldquo;Was hij zo groot als dit?&rdquo; &ldquo;Oh, veel groter!&rdquo; riepen ze. De Kikker blies zich nog meer op. &ldquo;Hij kon niet groter zijn dan dit,&rdquo; zei ze. Maar de kleine Kikkers verklaarden allemaal dat het monster veel, veel groter was en de oude Kikker bleef zich steeds meer opblazen totdat ze opeens barstte."
-}, { id: 2,
-    title: "De Kikker en de Os",
-    language: "Dutch",
-    imgUrl: "./images/story-thumbnail.jpg",
-    summary: "Een Os kwam naar een rietachtig poel om te drinken. Toen hij zwaar in het water plonsde, trapte hij een jonge Kikker in de modder. De oude Kikker miste het jonge diertje snel en vroeg aan zijn broers en zussen wat er van hem was geworden. &ldquo;Een grote monster,&rdquo; zei een van hen, &ldquo;stepte op onze kleine broer met een van zijn enorme voeten!&rdquo; &ldquo;Groot was hij!&rdquo; zei de oude Kikker, zich opblazend. &ldquo;Was hij zo groot als dit?&rdquo; &ldquo;Oh, veel groter!&rdquo; riepen ze. De Kikker blies zich nog meer op. &ldquo;Hij kon niet groter zijn dan dit,&rdquo; zei ze. Maar de kleine Kikkers verklaarden allemaal dat het monster veel, veel groter was en de oude Kikker bleef zich steeds meer opblazen totdat ze opeens barstte."
-}, { id: 3,
-    title: "De Kikker en de Os",
-    language: "Dutch",
-    imgUrl: "./images/story-thumbnail.jpg",
-    summary: "Een Os kwam naar een rietachtig poel om te drinken. Toen hij zwaar in het water plonsde, trapte hij een jonge Kikker in de modder. De oude Kikker miste het jonge diertje snel en vroeg aan zijn broers en zussen wat er van hem was geworden. &ldquo;Een grote monster,&rdquo; zei een van hen, &ldquo;stepte op onze kleine broer met een van zijn enorme voeten!&rdquo; &ldquo;Groot was hij!&rdquo; zei de oude Kikker, zich opblazend. &ldquo;Was hij zo groot als dit?&rdquo; &ldquo;Oh, veel groter!&rdquo; riepen ze. De Kikker blies zich nog meer op. &ldquo;Hij kon niet groter zijn dan dit,&rdquo; zei ze. Maar de kleine Kikkers verklaarden allemaal dat het monster veel, veel groter was en de oude Kikker bleef zich steeds meer opblazen totdat ze opeens barstte."
-}, { id: 4,
-    title: "De Kikker en de Os",
-    language: "Dutch",
-    imgUrl: "./images/story-thumbnail.jpg",
-    summary: "Een Os kwam naar een rietachtig poel om te drinken. Toen hij zwaar in het water plonsde, trapte hij een jonge Kikker in de modder. De oude Kikker miste het jonge diertje snel en vroeg aan zijn broers en zussen wat er van hem was geworden. &ldquo;Een grote monster,&rdquo; zei een van hen, &ldquo;stepte op onze kleine broer met een van zijn enorme voeten!&rdquo; &ldquo;Groot was hij!&rdquo; zei de oude Kikker, zich opblazend. &ldquo;Was hij zo groot als dit?&rdquo; &ldquo;Oh, veel groter!&rdquo; riepen ze. De Kikker blies zich nog meer op. &ldquo;Hij kon niet groter zijn dan dit,&rdquo; zei ze. Maar de kleine Kikkers verklaarden allemaal dat het monster veel, veel groter was en de oude Kikker bleef zich steeds meer opblazen totdat ze opeens barstte."
-}, { id: 5,
-    title: "De Kikker en de Os",
-    language: "Dutch",
-    imgUrl: "./images/story-thumbnail.jpg",
-    summary: "Een Os kwam naar een rietachtig poel om te drinken. Toen hij zwaar in het water plonsde, trapte hij een jonge Kikker in de modder. De oude Kikker miste het jonge diertje snel en vroeg aan zijn broers en zussen wat er van hem was geworden. &ldquo;Een grote monster,&rdquo; zei een van hen, &ldquo;stepte op onze kleine broer met een van zijn enorme voeten!&rdquo; &ldquo;Groot was hij!&rdquo; zei de oude Kikker, zich opblazend. &ldquo;Was hij zo groot als dit?&rdquo; &ldquo;Oh, veel groter!&rdquo; riepen ze. De Kikker blies zich nog meer op. &ldquo;Hij kon niet groter zijn dan dit,&rdquo; zei ze. Maar de kleine Kikkers verklaarden allemaal dat het monster veel, veel groter was en de oude Kikker bleef zich steeds meer opblazen totdat ze opeens barstte."
-}];
-
-// load stories on page load
-
-window.addEventListener("load", () => {
-    var stories = document.getElementById("stories");
-    var storyList = document.createElement("ul");
-    storyList.className = "story-list";
-    stories.appendChild(storyList);
-
-    
-    storyObject.map((story) => {
-        // create story list item
-        const storyListItem = document.createElement("li");
-        storyListItem.className = "story-list-item";
-        storyList.appendChild(storyListItem);
-      
-        // create story image
-        const storyImg = document.createElement("img");
-        storyImg.className = "story-img";
-        storyImg.src = story.imgUrl;
-        storyListItem.appendChild(storyImg);
-      
-        // create story info container
-        const storyInfo = document.createElement("div");
-        storyInfo.className = "story-info";
-        storyListItem.appendChild(storyInfo);
-
-            // create story title and append to info container
-            const storyTitle = document.createElement("h3");
-            storyTitle.className = "story-title";
-            storyTitle.innerHTML = story.title;
-            storyInfo.appendChild(storyTitle);
-      
-            // create story summary and append to info container
-            const storySummary = document.createElement("p");
-            storySummary.className = "story-summary";
-            storySummary.innerHTML = story.summary;
-            storyInfo.appendChild(storySummary);
-
-        // create story button append to list item
-        const storyButton = document.createElement("button");
-        storyButton.className = "story-button";
-        storyListItem.appendChild(storyButton);
-
-        // create story svg appearance and append to button
-        const iconSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        const iconPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        const iconCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-
-        iconSvg.classList.add('story-button-appearance');
-        iconSvg.setAttribute("fill", "none");
-        iconSvg.setAttribute("viewBox", "0 0 146 146");
-        iconSvg.setAttribute("width", "146");
-        iconSvg.setAttribute("height", "146");
-
-        iconPath.setAttribute(
-            "d",
-            "M57.0972 109.209C55.0372 110.511 52.9525 110.587 50.8431 109.437C48.7337 108.287 47.677 106.508 47.6729 104.101V41.9039C47.6729 39.5002 48.7296 37.7214 50.8431 36.5676C52.9567 35.4139 55.0413 35.49 57.0972 36.796L107.463 67.8943C109.317 69.0962 110.244 70.7988 110.244 73.0023C110.244 75.2057 109.317 76.9083 107.463 78.1102L57.0972 109.209Z"
-          );
-        iconPath.setAttribute("fill", "#FF7151");
-
-        iconCircle.setAttribute("cx", "73");
-        iconCircle.setAttribute("cy", "73");
-        iconCircle.setAttribute("r", "72");
-        iconCircle.setAttribute("stroke", "#FF7151");
-        iconCircle.setAttribute("stroke-width", "2");
-
-        iconSvg.appendChild(iconPath);
-        iconSvg.appendChild(iconCircle);
-        storyButton.appendChild(iconSvg);
-      });
-});
-
-const synth = window.speechSynthesis;
+import { renderStoryList } from "./storage.js";
+import { setupSpeechSynthesis } from "./synthesizer.js";
 
 window.addEventListener('load', () => {
-    const storyList = document.querySelector('.story-list');
 
-  if (storyList) {
-    storyList.addEventListener('click', (e) => {
-      const listItem = e.target.closest('.story-list-item');
+  const currentStoryList = renderStoryList();
+  const speak = setupSpeechSynthesis();
+});
 
-      // Check if synth is speaking
-      if (synth.speaking) {
-            synth.cancel();
-            return;
-      }
+// SOURCE: chat.openai.com/chat
+const navLink = document.querySelector('a[href="#stories"]');
 
-      if (listItem) {
-        const listItems = Array.from(storyList.children);
-        const index = listItems.indexOf(listItem) + 1;
+navLink.addEventListener('click', (e) => {
+    e.preventDefault();
 
-        const story = storyObject.find((story) => story.id === index);
-        const summaryUtterance = new SpeechSynthesisUtterance(story.summary);
+    const target = document.querySelector(navLink.getAttribute('href'));
+    const targetPosition = target.getBoundingClientRect().top;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 1500; // Adjust this value to set the scroll speed
+    let start = null;
 
-        summaryUtterance.onend = (event) => {
-            console.log("SpeechSynthesisUtterance.onend");
-          };
-      
-        summaryUtterance.onerror = (event) => {
-            console.error("SpeechSynthesisUtterance.onerror" + " " + event.error);
-          };
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      const progress = timestamp - start;
+      const percent = Math.min(progress / duration, 1);
+      window.scrollTo(0, startPosition + distance * easeInOut(percent));
+      if (progress < duration) window.requestAnimationFrame(step);
+    }
 
-        synth.speak(summaryUtterance);
-      } else{
-            console.log("Not a story summary");
-      }
-    });
-  }
+    function easeInOut(t) {
+      // Use a cubic-bezier curve for easing with an ease-in-out effect
+      return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * (t - 1)) * (2 * (t - 1)) + 1;
+    }
+
+
+    window.requestAnimationFrame(step);
 });
---
+++ scripts/synthesizer.js
@@ -0,0 +1,38 @@
+import { storyObject } from "./storage.js";
+
+export function setupSpeechSynthesis() {
+    const synth = window.speechSynthesis;
+    const storyList = document.querySelector('.story-list');
+  
+    if (storyList) {
+      storyList.addEventListener('click', (e) => {
+        const listItem = e.target.closest('.story-list-item');
+  
+        // Check if synth is speaking
+        if (synth.speaking) {
+          synth.cancel();
+          return;
+        }
+  
+        if (listItem) {
+          const listItems = Array.from(storyList.children);
+          const index = listItems.indexOf(listItem) + 1;
+  
+          const story = storyObject.find((story) => story.id === index);
+          const summaryUtterance = new SpeechSynthesisUtterance(story.summary);
+  
+          summaryUtterance.onend = (event) => {
+            console.log("SpeechSynthesisUtterance.onend");
+          };
+  
+          summaryUtterance.onerror = (event) => {
+            console.error("SpeechSynthesisUtterance.onerror" + " " + event.error);
+          };
+  
+          synth.speak(summaryUtterance);
+        } else {
+          console.log("Not a story summary");
+        }
+      });
+    }
+  }
