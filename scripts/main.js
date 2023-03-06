549e8fb432b7f52ddef9236e7bce28b9395b19cd
Michelwas97 <mgwassing@gmail.com>
2023-03-06 13:40:41 -0400

    added: routie for routing possibilities

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,13 @@
 import { renderStoryList } from "./storage.js";
 import { setupSpeechSynthesis } from "./synthesizer.js";
 
-window.addEventListener('load', () => {
-
-  const currentStoryList = renderStoryList();
-  const speak = setupSpeechSynthesis();
+document.addEventListener('DOMContentLoaded', async () => {
+  routie('stories', function() {
+    //this gets called when hash == #stories
+    renderStoryList();
+  });
 });
 
-// SOURCE: chat.openai.com/chat
-const navLink = document.querySelector('a[href="#stories"]');
-
-navLink.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const target = document.querySelector(navLink.getAttribute('href'));
-    const targetPosition = target.getBoundingClientRect().top;
-    const startPosition = window.pageYOffset;
-    const distance = targetPosition - startPosition;
-    const duration = 1500; // Adjust this value to set the scroll speed
-    let start = null;
-
-    function step(timestamp) {
-      if (!start) start = timestamp;
-      const progress = timestamp - start;
-      const percent = Math.min(progress / duration, 1);
-      window.scrollTo(0, startPosition + distance * easeInOut(percent));
-      if (progress < duration) window.requestAnimationFrame(step);
-    }
-
-    function easeInOut(t) {
-      // Use a cubic-bezier curve for easing with an ease-in-out effect
-      return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * (t - 1)) * (2 * (t - 1)) + 1;
-    }
-
-
-    window.requestAnimationFrame(step);
+window.addEventListener('load', () => {
+  const speak = setupSpeechSynthesis();
 });
